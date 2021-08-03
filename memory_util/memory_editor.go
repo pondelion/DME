8c4c2ca1805bd63783aac4a43e73a1b5df7a51f0
pondelion <programming.deve@gmail.com>
2021-08-03 09:43:12 +0900

    fix

--- memory_util/memory_editor.go
+++ memory_util/memory_editor.go
@@ -9,10 +9,11 @@ import (
 
 func WriteProcMemInt(pid int, addr int64, value int64) {
 	filepath := fmt.Sprintf("/proc/%d/mem", pid)
-	file, err := os.Open(filepath)
+	file, err := os.OpenFile(filepath, os.O_RDWR, 0644)
 	if err != nil {
 		panic(err)
 	}
+	defer file.Close()
 	err = syscall.PtraceAttach(pid)
 	if err != nil {
 		fmt.Println(err)
@@ -23,14 +24,15 @@ func WriteProcMemInt(pid int, addr int64, value int64) {
 	var whence int = 0
 	_, err = file.Seek(addr, whence)
 	if err != nil {
-		err = syscall.PtraceDetach(pid)
+		_ = syscall.PtraceDetach(pid)
 		panic(err)
 	}
-	byte_data := make([]byte, binary.MaxVarintLen64)
-	binary.PutVarint(byte_data, value)
+	byte_data := make([]byte, 8)
+	binary.LittleEndian.PutUint64(byte_data, uint64(value))
+	// log.Printf("%d", int64(binary.LittleEndian.Uint64(byte_data)))
 	_, err = file.Write(byte_data)
 	if err != nil {
-		err = syscall.PtraceDetach(pid)
+		_ = syscall.PtraceDetach(pid)
 		panic(err)
 	}
 	err = syscall.PtraceDetach(pid)
